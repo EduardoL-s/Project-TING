6620f77b9a80f88a7e28eb85fb44138ec378b380
EduardoL-s <eduardoluizmm@hotmail.com>
2023-07-06 18:10:02 -0300

    [Requisito 8] Implementação da opção de retornar tanto a linha quanto a posição dela quando procurar por uma palavra específica

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -11,7 +11,7 @@ def exists_word(word, instance):
 
         for line_index, line in enumerate(result["linhas_do_arquivo"]):
             if word.lower() in line.lower():
-                word_search_dict["ocorrencias"].append({"linha": line_index + 1})
+                word_search_dict["ocorrencias"].append({"linha": line_index+1})
 
         if len(word_search_dict["ocorrencias"]) > 0:
             result_exists_word.append(word_search_dict)
@@ -19,4 +19,23 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result_exists_word = []
+
+    for index in range(len(instance)):
+        result = instance.search(index)
+        word_search_dict = {
+            "palavra": word,
+            "arquivo": result["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for line_index, line in enumerate(result["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                word_search_dict["ocorrencias"].append({
+                    "linha": line_index+1,
+                    "conteudo": line,
+                    })
+
+        if len(word_search_dict["ocorrencias"]) > 0:
+            result_exists_word.append(word_search_dict)
+    return result_exists_word
