2ef7e1436fd15370df25045b59193b0acf6406ec
EduardoL-s <eduardoluizmm@hotmail.com>
2023-07-06 17:49:23 -0300

    [Requisito 7] Implementação da função que procura por uma palavra em específico presente nas instâncias

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result_exists_word = []
+
+    for index in range(len(instance)):
+        result = instance.search(index)
+        word_search_dict = {
+            "palavra": word,
+            "arquivo": result["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for line_index, line in enumerate(result["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                word_search_dict["ocorrencias"].append({"linha": line_index + 1})
+
+        if len(word_search_dict["ocorrencias"]) > 0:
+            result_exists_word.append(word_search_dict)
+    return result_exists_word
 
 
 def search_by_word(word, instance):
